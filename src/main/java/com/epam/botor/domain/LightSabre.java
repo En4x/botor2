0f302faf39606030c2ac9fc7e33ab8091b0e9a6a
Peter Veres <peter_veres2@epam.com>
2016-08-15 10:36:47 +0200

    domain

---
+++ src/main/java/com/epam/botor/domain/LightSabre.java
@@ -0,0 +1,10 @@
+package com.epam.botor.domain;
+
+public class LightSabre extends Weapon implements EnergyWeapon {
+
+    public LightSabre(final String name, final int damage) {
+        super(name, damage);
+        // TODO Auto-generated constructor stub
+    }
+
+}
