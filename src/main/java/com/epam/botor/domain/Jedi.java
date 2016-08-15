1101d94015a4ff09b68eea5355f0fcb45a4f4b56
Peter Veres <peter_veres2@epam.com>
2016-08-15 14:33:32 +0200

    Domain model

--- src/main/java/com/epam/botor/domain/Jedi.java
+++ src/main/java/com/epam/botor/domain/Jedi.java
@@ -1,15 +1,21 @@
 package com.epam.botor.domain;
 
-public class Jedi {
+public class Jedi extends Person implements ForceUser {
 
 	private final LightSabre lightSabre;
-
-	public Jedi(LightSabre lightSabre) {
+	
+    public Jedi(String name, Gender gender, LightSabre lightSabre) {
+		super(name, gender);
 		this.lightSabre = lightSabre;
 	}
-	
+
+	@Override
+    public ForceSide getAlliance() {
+        return ForceSide.LIGHT;
+    }
+
 	public String fight() {
 		return lightSabre.hit();
 	}
-	
+
 }
--- src/main/java/com/epam/botor/domain/LightSabre.java
+++ src/main/java/com/epam/botor/domain/LightSabre.java
@@ -1,36 +1,21 @@
 package com.epam.botor.domain;
 
-public class LightSabre {
-	private final String name;
-	private final int damage;
-	
-	public LightSabre(String name, int damage) {
-		super();
-		this.name = name;
-		this.damage = damage;
-		System.out.printf("Light sabre '%s' created.\n", name);
+public class LightSabre extends Weapon implements EnergyWeapon {
+
+    public LightSabre(final String name, final int damage) {
+        super(name, damage);
+        // TODO Auto-generated constructor stub
+    }
+
+	public int getIdentityHashCode() {
+		return System.identityHashCode(this);
 	}
-	
+
 	public String hit() {
-		return "tyútyútyútyú";
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public int getDamage() {
-		return damage;
-	}
-	
-	public String getIdentityHashCode() {
-		return "" + System.identityHashCode(this);
-	}
-
-	@Override
-	public String toString() {
-		return "LightSabre [name=" + name + ", damage=" + damage + "]";
+		return "Tyutyutyu";
 	}
 	
 	
+	
+
 }
