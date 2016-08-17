a5a4b4190cedd7b4e61c88d3676dfd5e925873d2
Peter Veres <peter_veres2@epam.com>
2016-08-17 14:00:05 +0200

    @Configuration #3

--- src/main/java/com/epam/botor/config/BlasterConfiguration.java
+++ src/main/java/com/epam/botor/config/BlasterConfiguration.java
@@ -3,6 +3,7 @@ package com.epam.botor.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 import com.epam.botor.domain.Blaster;
 import com.epam.botor.domain.BlasterPistol;
@@ -14,5 +15,11 @@ public class BlasterConfiguration {
 	@Bean
 	public Blaster commonBlaster(@Value("blaster") String name, @Value("100") int damage) {
 		return Math.random() > 0.5 ? new BlasterPistol(name, damage) : new BlasterRifle(name, damage);
+	}
+	
+	@Bean
+	@Scope("prototype")
+	public BlasterPistol blasterPistol() {
+		return new BlasterPistol("Blaster pistol", (int)(95 + Math.random() * 10));
 	}
 }
