f059048af82368281b5304217d64f43075a6079f
Peter Veres <peter_veres2@epam.com>
2016-08-17 09:23:25 +0200

    PropertyPlaceholderConfigurer

--- src/main/java/com/epam/botor/App.java
+++ src/main/java/com/epam/botor/App.java
@@ -25,7 +25,7 @@ public class App {
 			Battle battle = context.getBean(Battle.class);
 			
 			battle.battle();
-			LOGGER.debug("" + context.getBean("obiVanKenobi", Jedi.class).getPadavan());
+			LOGGER.debug("" + context.getBean("redSabre"));
 			
 		}
 		
