5b220ff16ce149367c7816c3428dec44011399b8
Peter Veres <peter_veres2@epam.com>
2016-08-18 14:03:58 +0200

    Advice ordering

--- src/main/java/com/epam/botor/aspects/EnergySaverLightSabreAspect.java
+++ src/main/java/com/epam/botor/aspects/EnergySaverLightSabreAspect.java
@@ -7,6 +7,8 @@ import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import com.epam.botor.domain.LightSabre;
@@ -18,17 +20,21 @@ public class EnergySaverLightSabreAspect {
     private static final Logger LOGGER = LoggerFactory.getLogger(EnergySaverLightSabreAspect.class);
 
     @Before("lightSabreInUse()")
+    @Order(100)
     public void switchOnBefore(final JoinPoint joinPoint) {
 
         LightSabre targetLightSabre = (LightSabre) joinPoint.getTarget();
 
         LOGGER.warn("Switching on " + targetLightSabre.getName());
+        LOGGER.warn(joinPoint.getTarget().getClass().getName());
+        LOGGER.warn(joinPoint.getThis().getClass().getName());
 
         targetLightSabre.setSwitchedOn(true);
 
     }
 
     @After("lightSabreInUse()")
+    @Order(100)
     public void switchOffAfter(final JoinPoint joinPoint) {
 
         LightSabre targetLightSabre = (LightSabre) joinPoint.getTarget();
@@ -40,7 +46,7 @@ public class EnergySaverLightSabreAspect {
     }
 
     @Pointcut("execution (String com.epam.botor.domain.LightSabre.shoot())")
-    private void lightSabreInUse() {
+    protected void lightSabreInUse() {
 
     }
 
---
+++ src/main/java/com/epam/botor/aspects/EnergySaverLightSabreTester.java
@@ -0,0 +1,40 @@
+package com.epam.botor.aspects;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import com.epam.botor.domain.LightSabre;
+
+@Aspect
+@Component
+public class EnergySaverLightSabreTester {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(EnergySaverLightSabreTester.class);
+
+    @Before("com.epam.botor.aspects.EnergySaverLightSabreAspect.lightSabreInUse()")
+    @Order(200)
+    public void testSwitchedOnBefore(final JoinPoint joinPoint) {
+
+        LightSabre targetLightSabre = (LightSabre) joinPoint.getTarget();
+        LOGGER.warn(joinPoint.getTarget().getClass().getName());
+        LOGGER.warn(joinPoint.getThis().getClass().getName());
+        LOGGER.warn("Switched on before use: " + targetLightSabre.isSwitchedOn());
+
+    }
+
+    @After("com.epam.botor.aspects.EnergySaverLightSabreAspect.lightSabreInUse()")
+    @Order(200)
+    public void testSwitchedOnAfter(final JoinPoint joinPoint) {
+
+        LightSabre targetLightSabre = (LightSabre) joinPoint.getTarget();
+
+        LOGGER.warn("Switched on after use: " + targetLightSabre.isSwitchedOn());
+
+    }
+}
